808729ea33c4164ecba826eb0b582b4d9436267d
syt123450 <421537891@qq.com>
2019-03-07 19:54:52 -0800

    add yolo and data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,174 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import * as TSP from 'tensorspace';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	
+	componentDidMount() {
+		
+		let modelContainer = document.getElementById( "container" );
+
+		let model = new TSP.models.Sequential( modelContainer, {
+
+			stats: true,
+			animeTime: 200
+
+		}  );
+
+		model.add( new TSP.layers.RGBInput( {
+
+			shape: [ 416, 416, 3 ]
+
+		} ) );
+
+		model.add( new TSP.layers.Conv2d( {
+
+			kernelSize: 3,
+			filters: 16,
+			strides: 1,
+			padding: "same"
+
+		} ) );
+
+		model.add( new TSP.layers.Pooling2d( {
+
+			poolSize: [ 2, 2 ],
+			strides: [ 2, 2 ]
+
+		} ) );
+
+		model.add( new TSP.layers.Conv2d( {
+
+			kernelSize: 3,
+			filters: 32,
+			strides: 1,
+			padding: "same"
+
+		} ) );
+
+		model.add( new TSP.layers.Pooling2d( {
+
+			poolSize: [ 2, 2 ],
+			strides: [ 2, 2 ]
+
+		} ) );
+
+		model.add( new TSP.layers.Conv2d( {
+
+			kernelSize: 3,
+			filters: 64,
+			strides: 1,
+			padding: "same"
+
+		} ) );
+
+		model.add( new TSP.layers.Pooling2d( {
+
+			poolSize: [ 2, 2 ],
+			strides: [ 2, 2 ]
+
+		} ) );
+
+		model.add( new TSP.layers.Conv2d( {
+
+			kernelSize: 3,
+			filters: 128,
+			strides: 1,
+			padding: "same"
+
+		} ) );
+
+		model.add( new TSP.layers.Pooling2d( {
+
+			poolSize: [ 2, 2 ],
+			strides: [ 2, 2 ]
+
+		} ) );
+
+		model.add( new TSP.layers.Conv2d( {
+
+			kernelSize: 3,
+			filters: 256,
+			strides: 1,
+			padding: "same"
+
+		} ) );
+
+		model.add( new TSP.layers.Pooling2d( {
+
+			poolSize: [ 2, 2 ],
+			strides: [ 2, 2 ]
+
+		} ) );
+
+		model.add( new TSP.layers.Conv2d( {
+
+			kernelSize: 3,
+			filters: 512,
+			strides: 1,
+			padding: "same"
+
+		} ) );
+
+		model.add( new TSP.layers.Pooling2d( {
+
+			poolSize: [ 2, 2 ],
+			strides: [ 1, 1 ],
+			padding: "same"
+
+		} ) );
+
+		model.add( new TSP.layers.Conv2d( {
+
+			kernelSize: 3,
+			filters: 1024,
+			strides: 1,
+			padding: "same"
+
+		} ) );
+
+		model.add( new TSP.layers.Conv2d( {
+
+			kernelSize: 3,
+			filters: 512,
+			strides: 1,
+			padding: "same"
+
+		} ) );
+
+		model.add( new TSP.layers.Conv2d( {
+
+			kernelSize: 1,
+			filters: 125,
+			strides: 1
+
+		} ) );
+
+		model.load( {
+
+			type: "tensorflow",
+			url: "./assets/model/model.json",
+			outputsName: [ "Maximum", "MaxPool", "Maximum_1", "MaxPool_1", "Maximum_2",
+				"MaxPool_2", "Maximum_3", "MaxPool_3", "Maximum_4", "MaxPool_4",
+				"Maximum_5", "MaxPool_5", "Maximum_6", "Maximum_7", "add_8" ],
+		} );
+		
+		model.init( function() {
+
+			fetch('./assets/data/ycy_1.json').then(res => res.json()).then(data => {
+				model.predict(data);
+			});
+
+		} );
+		
+	}
+	
+	render() {
+		return (
+			<div id={'container'}>
+			</div>
+		);
+	}
 }
 
 export default App;
